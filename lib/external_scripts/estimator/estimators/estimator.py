c977fec3eb406054fbb21ca19b3c62f89e21caf3
Eric Youngberg <eric@lmtlss.net>
2017-06-21 15:09:32 -0400

    Load multiple data files into hashtable that is passed to the methodology

--- lib/external_scripts/estimator/estimators/estimator.py
+++ lib/external_scripts/estimator/estimators/estimator.py
@@ -4,8 +4,11 @@ import pandas as pd
 class Estimator(object):
 
   def __new__(self, fn):
-    def estimator(data_source):
-      data = pd.read_excel(data_source)
+    def estimator(data_sources):
+      data = {}
+
+      for data_source in data_sources:
+        data[data_source] = pd.read_excel(data_source)
 
       return fn(data)
 
