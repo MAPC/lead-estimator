01d39ca1ad9d548d41756e3f75d2e1fe158cfec9
Eric Youngberg <eric@lmtlss.net>
2017-06-22 11:23:09 -0400

    Finish Estimator to compose methodologies and data sources together to processing

--- lib/external_scripts/estimator/estimators/estimator.py
+++ lib/external_scripts/estimator/estimators/estimator.py
@@ -3,6 +3,8 @@ from os import path
 
 class Estimator(object):
 
+  loaded_files = {}
+
   def __new__(self, fn):
     def estimator(data_sources):
       data = {}
@@ -14,8 +16,11 @@ class Estimator(object):
       }
 
       for data_source in data_sources:
-        file_type = path.splitext(data_source['file_path'])[1][1:]
-        data[data_source['tag']] = file_readers[file_type](data_source['file_path'])
+        if not data_source['tag'] in Estimator.loaded_files:
+          file_type = path.splitext(data_source['file_path'])[1][1:]
+          Estimator.loaded_files[data_source['tag']] = file_readers[file_type](data_source['file_path'])
+
+        data[data_source['tag']] = Estimator.loaded_files[data_source['tag']]
 
       return fn(data)
 
--- lib/external_scripts/estimator/estimators/industrial.py
+++ lib/external_scripts/estimator/estimators/industrial.py
@@ -1,2 +1,8 @@
-def industrial(data_file):
-  return
+from .estimator import Estimator
+
+def industrial(data_sources):
+
+  def methodology(data):
+    return data
+
+  return Estimator(methodology)(data_sources)
