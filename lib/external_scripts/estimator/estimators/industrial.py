c902e108027997235a8ecf5d2e40beb05eb89d24
Eric Youngberg <eric@lmtlss.net>
2017-07-03 14:07:13 -0400

    Finish Industrial estimator

--- lib/external_scripts/estimator/estimators/industrial.py
+++ lib/external_scripts/estimator/estimators/industrial.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
 from pprint import pprint
 from .estimator import Estimator
 
 def industrial(data_sources):
+
+  fuel_types = ['elec', 'foil', 'ng', 'other']
+
+  def replace_invalid_values(df):
+    df.replace('*', np.nan, inplace=True)
+    df.replace('Q', np.nan, inplace=True)
+
 
   def methodology(datasets):
 
@@ -10,9 +18,8 @@ def industrial(data_sources):
       Step 1 in Methodology
     """
     eowld = datasets['eowld']
-    eowld = eowld[eowld['municipal'].str.lower() == 'gloucester']
     eowld = eowld[(eowld['naicscode'].astype(int) >= 311) & (eowld['naicscode'].astype(int) <= 339) & (eowld['cal_year'].astype(int) == 2015)]
-    eowld = eowld[['naicscode', 'naicstitle', 'avgemp', 'estab']]
+    eowld = eowld[['naicscode', 'naicstitle', 'municipal', 'avgemp', 'estab']]
     eowld = eowld.sort_values(['naicscode']) 
     eowld.rename(columns={'naicscode': 'naics_code'}, inplace=True)
 
@@ -31,21 +38,30 @@ def industrial(data_sources):
     mecs_fce = mecs_fce[['naics_code', 'cons_emp']]
 
     results = pd.merge(results, mecs_fce, on='naics_code')
+    replace_invalid_values(results)
     results['cons_total'] = results['cons_emp'].str.replace(',','').astype(float) * results['avgemp'].astype(float)
 
 
     """
       Step 3 in Methodology
     """
-    mecs_ami = datasets['mecs_ami']
+    mecs_ami = pd.DataFrame(datasets['mecs_ami'])
     mecs_ami['naics_code'] = mecs_ami['naics_code'].apply(pd.to_numeric, errors='coerce')
-    mecs_ami = mecs_ami[mecs_ami['naics_code'].isin(naics_codes)]
+    mecs_ami = mecs_ami[(mecs_ami['naics_code'].isin(naics_codes)) & (mecs_ami['geography'].str.lower() == 'northeast')]
+    replace_invalid_values(mecs_ami)
 
-    #mecs_ami['cons_total'] = mecs_ami[['elec', 'res_foil', '']]
+    mecs_ami['other'] = mecs_ami[['lpgngl', 'coal', 'coke', 'other']].apply(pd.to_numeric).sum(axis=1, skipna=True)
+    mecs_ami['foil'] = mecs_ami[['dist_foil', 'res_foil']].apply(pd.to_numeric).sum(axis=1, skipna=True)
+    mecs_ami = mecs_ami[fuel_types + ['tot', 'naics_code']]
 
-    pprint(mecs_ami)
+    for fuel in fuel_types:
+      mecs_ami[fuel+'_%'] = mecs_ami[fuel].astype(float) / mecs_ami['tot'].astype(float)
 
-    #mecs_fci = mecs_fci[(mecs_fci['naics_code'].isin(naics_codes)) & (mecs_fci['region'].str.lower() == 'northeast')]
+    mecs_ami.drop(fuel_types + ['tot'], axis=1, inplace=True)
+    results = pd.merge(results, mecs_ami, on='naics_code')
+
+    for fuel in fuel_types:
+      results[fuel+'_cons'] = results['cons_total'] * results[fuel+'_%']
 
 
     return results
